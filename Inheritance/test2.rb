00219e1fb718e7628e7e21769a7a904a8df94008
Chethan <crajendra@qwinix.io>
2016-10-07 17:41:26 +0530

    Test

--- Inheritance/test2.rb
+++ Inheritance/test2.rb
@@ -1,3 +1,4 @@
+
 # The user wants to call his friend who is in New York (USA).
 # The user has his friends New York landline number.
 # To make a call the user has to add the country code and the city code to the landline number. 
@@ -36,30 +37,37 @@ class Country
 		puts "This is the Country code."
 	end
 
-	def countryCode 
+	def countryCode
+	print 00 
 	 # the country code is 00
 	end
 end
 
-class City
+class City < Country
 	def initialize
 		puts "This is the City code."
 	end
-
+     def cityCode
+     	print 212
+     end
 	# define cityCode  --> city code is 212
 end
 
 
-class Number < Country
+class Number < City
 	def initialize
 		puts "This is the landline number."
+	end
+	def phoneNumber
+		print 2414211
+		puts
 	end
 
 	# define phoneNumber --> phone number is 2414211
 
 end
 
-class Number < City
+class Numberof < Number
 	def initialize
 		puts "This is the landline number."
 	end
@@ -67,10 +75,18 @@ class Number < City
 	# define phoneNumber --> phone number is 2414211
 
 	def dialNumber
-		puts countryCode cityCode phoneNumber 
+		 
 	end
 end
 
-
-
 # instantiate a object and check for all code and final number
+
+
+
+# ob2=Country.new
+# ob2.countryCode
+
+ob1=Numberof.new
+ob1.countryCode
+ob1.cityCode
+ob1.phoneNumber
